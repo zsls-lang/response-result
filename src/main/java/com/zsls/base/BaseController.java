e8899c38436b2a465fc625b0a0978d87db7118cc
zk1023lang <zk1023@163.com>
2020-12-24 16:54:00 +0800

    增加一些基础类

---
+++ src/main/java/com/zsls/base/BaseController.java
@@ -0,0 +1,12 @@
+package com.zsls.base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *@ClassName BaseController
+ *@Version 1.0
+ */
+public abstract class BaseController {
+	protected Logger logger = LoggerFactory.getLogger(this.getClass());
+}
